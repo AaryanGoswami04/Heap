0f3d3d7e72d5576ad8d1632c2961c847f292daef
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-18 22:16:33 +0530

    Update and rename main (14).cpp to HeapSort.cpp

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
+//Time complexity:O(N log N)
+//Space complexity: O(1)
+//1-based indexing
 void heapify(int arr[], int n, int i){
     int largest = i;
-    int left = 2*i;  //left child
+    int left = 2*i;  //left child 
     int right = 2*i+1;   //right child
         if(left < n && arr[largest]<arr[left])
            largest = left;
@@ -10,27 +13,25 @@ void heapify(int arr[], int n, int i){
         if(right < n && arr[largest]<arr[right])
             largest = right;
     
-        if(largest != i){   //If the smallest value at the subtree is not at root, then swap the root and the smallest value 
+        if(largest != i){   //If the largest value at the subtree is not at root, then swap the root and the largest value 
            swap(arr[largest], arr[i]);
-           heapify(arr, n, largest);  //Heapify the node which contained the smallest value
+           heapify(arr, n, largest);  //Heapify the node which contained the largest value
         }
 }
 void HeapSort(int arr[], int n){
   int size=n;
   while(size>1){
-    //swap
+    //Step 1:swap root and last node i.e the max element of array is moved to its correct place
     swap(arr[size],arr[1]);
     size--;
 
-    //Step 2
+    //Step 2: Heapify the root
     heapify(arr, size, 1);
   }
 }
 int main() {
+  //arr follows 1 based indexing
  int arr[6]={-1,4,10,18,3,20};
 int n=5;
-//heap creation
-for(int i=n/2;i>0;i--)
-   heapify(arr, n, i);
 HeapSort(arr,n);
 }
