b3887c1952799e1aa4802c6423478c68140a38b7
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-20 16:39:33 +0530

    Update and rename main (17).cpp to K-thLargestSubarraySum.cpp

--- K-thLargestSubarraySum.cpp
+++ K-thLargestSubarraySum.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+//TC: N^2 log(K)
+//SC:O(K)
 int getKthLargest(vector<int> &arr, int k)
 {
-    priority_queue<int,vector<int>,greater<int>>mini;
+    priority_queue <int,vector<int>,greater<int>> mini;
     int n=arr.size();
     vector<int>sumstore;
+    
     for(int i=0; i<n; i++){
         int sum=0;
         for(int j=i; j<n; j++){
-            sum+=arr[j];
+            sum += arr[j];
             if(mini.size()<k)
                     mini.push(sum);
             else{
@@ -19,7 +22,4 @@ int getKthLargest(vector<int> &arr, int k)
         }
     }
         return mini.top();
-}
-int main() {
-  std::cout << "Hello World!\n";
 }
