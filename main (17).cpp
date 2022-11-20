3c62c90aa4bb7ca889bb67126d7f0f9af180e5b1
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-20 16:38:12 +0530

    Add files via upload

---
+++ main (17).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int getKthLargest(vector<int> &arr, int k)
+{
+    priority_queue<int,vector<int>,greater<int>>mini;
+    int n=arr.size();
+    vector<int>sumstore;
+    for(int i=0; i<n; i++){
+        int sum=0;
+        for(int j=i; j<n; j++){
+            sum+=arr[j];
+            if(mini.size()<k)
+                    mini.push(sum);
+            else{
+                if(mini.top() < sum){
+                    mini.pop();
+                    mini.push(sum);
+                }
+            }
+        }
+    }
+        return mini.top();
+}
+int main() {
+  std::cout << "Hello World!\n";
+}
