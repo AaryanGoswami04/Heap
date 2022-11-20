c8757bf5ca4d75203cad7637c5fb05c1a2fc3f96
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-20 18:03:00 +0530

    Update K-thLargestSubarraySum.cpp

--- K-thLargestSubarraySum.cpp
+++ K-thLargestSubarraySum.cpp
@@ -5,21 +5,16 @@ int getKthLargest(vector<int> &arr, int k)
 {
     priority_queue <int,vector<int>,greater<int>> mini;
     int n=arr.size();
-    vector<int>sumstore;
     
     for(int i=0; i<n; i++){
         int sum=0;
         for(int j=i; j<n; j++){
             sum += arr[j];
-            if(mini.size()<k)
-                    mini.push(sum);
-            else{
-                if(mini.top() < sum){
-                    mini.pop();
-                    mini.push(sum);
-                }
+            mini.push(sum);
+            
+            if(mini.size()>k)
+                mini.pop();
             }
         }
-    }
-        return mini.top();
+     return mini.top();
 }
